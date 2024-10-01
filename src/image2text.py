7382468386ed1b1f3be900cfd70969d9129348f4
Andrew Karpich <andrewkarpich@gmail.com>
2024-10-02 01:24:53 +0300

    Fixed missing nodes

--- src/image2text.py
+++ src/image2text.py
@@ -66,7 +66,7 @@ class Image2Text:
         return {
             "required": {
                 "model": ("IMAGE2TEXT_MODEL", ),
-                "image": ("IMAGE", "IMAGE_URL"),
+                "image": ("IMAGE", ),
                 "query": (["Describe this photograph.","What is this?","Please describe this image in detail.",cls.QUERY_EXPERT_TAGS], {
                     "default": "What is this?",
                     "multiline": True,
