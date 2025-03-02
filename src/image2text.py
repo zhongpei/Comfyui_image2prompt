7ff1018ee50c8cee1a43e9d83e691e1410e58a5d
Yuki <50353278+YukihimeX@users.noreply.github.com>
2025-03-02 01:48:03 -0600

    data: YukihimeX
    patch 'str' object has no attribute 'get' based on https://github.com/zhongpei/Comfyui_image2prompt/issues/87#issuecomment-2569398110

--- src/image2text.py
+++ src/image2text.py
@@ -66,7 +66,7 @@ class Image2Text:
         return {
             "required": {
                 "model": ("IMAGE2TEXT_MODEL", ),
-                "image": ("IMAGE", "IMAGE_URL"),
+                "image": ("IMAGE",), #("IMAGE", "IMAGE_URL"),
                 "query": (["Describe this photograph.","What is this?","Please describe this image in detail.",cls.QUERY_EXPERT_TAGS], {
                     "default": "What is this?",
                     "multiline": True,
